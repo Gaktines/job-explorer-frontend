a4382231b23f79acfd0075d3c26d83ce39338cd6
Gaktines <119362033+Gaktines@users.noreply.github.com>
2023-12-05 12:53:13 -0700

    jsupdates

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -4,26 +4,14 @@ import SideBar from "../SideBar/SideBar";
 
 
 const Profile = ({
-  onSelectCard,
-  handleActiveCreateModal,
-  selectedCard,
   handleEditModal,
   handleLogout,
-  loggedIn,
-  onCardLike,
   onSubmit,
 }) => {
   return (
     <section className="profile">
       <SideBar handleEditModal={handleEditModal} handleLogout={handleLogout} onSubmit={onSubmit}/>
-      <JobSection
-        onSelectCard={onSelectCard}
-        handleActiveCreateModal={handleActiveCreateModal}
       
-        selectedCard={selectedCard}
-        loggedIn={loggedIn}
-        onCardLike={onCardLike}
-      />
     </section>
   );
 };
