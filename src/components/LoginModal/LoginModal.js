bb65bb90163c76624d51da812d3525f62cd43b1d
Gaktines <119362033+Gaktines@users.noreply.github.com>
2023-12-06 14:10:57 -0700

    jsupdates

--- src/components/LoginModal/LoginModal.js
+++ src/components/LoginModal/LoginModal.js
@@ -35,34 +35,30 @@ const LoginModal = ({
       onClose={handleCloseModal}
     >
       <h2>Log in</h2>
-      <label className="modal__label">
-        Email
-        <input
-          className="modal__email"
-          type="email"
-          name="email"
-          placeholder="Email"
-          required
-          value={email}
-          onChange={handleEmailChange}
-        ></input>
-      </label>
-      <label className="modal__label">
-        Password
-        <input
-          className="modal__password"
-          type="text"
-          name="password"
-          placeholder="Password"
-          minLength="1"
-          maxLength="8"
-          required
-          value={password}
-          onChange={handlePasswordChange}
-        ></input>
-      </label>
+
+      <input
+        className="modal__email"
+        type="email"
+        name="email"
+        placeholder="Email"
+        required
+        value={email}
+        onChange={handleEmailChange}
+      ></input>
+
+      <input
+        className="modal__password"
+        type="text"
+        name="password"
+        placeholder="Password"
+        minLength="1"
+        maxLength="8"
+        required
+        value={password}
+        onChange={handlePasswordChange}
+      ></input>
+
       <div className="modal__button-container">
-        
         <button
           className="modal__submit-register-button"
           type="button"
