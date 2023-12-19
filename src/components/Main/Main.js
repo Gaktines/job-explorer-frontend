2275e7e4b225366bd5e75be4175b57d07c0dd59d
Gaktines <119362033+Gaktines@users.noreply.github.com>
2023-12-19 10:39:05 -0700

    jsupdatea

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,26 +4,23 @@ import JobCard from "../JobCard/JobCard";
 
 function Main({ onSelectCard, jobItems, setSelectedCard, loggedIn }) {
   const current = new Date();
-  const date = `${current.getFullYear()}-${current.getMonth()+1}-${current.getDate()}`;
+  const date = `${current.getFullYear()}-${
+    current.getMonth() + 1
+  }-${current.getDate()}`;
 
-const sortedCards = jobItems?.sort((a, b) => 
-    new Date(a.jobPostingDate) - new Date(b.jobPostingDate)
-  
-);
-   
-
-
+  const sortedCards = jobItems?.sort(
+    (a, b) => new Date(a.jobPostingDate) - new Date(b.jobPostingDate)
+  );
 
   return (
     <main className="main">
       <section className="main__card-section">
         Today is {date}
         <div className="main__cards">
-        {sortedCards.map((x) => (
-          
+          {sortedCards.map((x) => (
             <JobCard
-            
-            item={x}
+              item={x}
+              key={x.jobLink}
               onSelectCard={onSelectCard}
               onClick={() => {
                 setSelectedCard(x);
